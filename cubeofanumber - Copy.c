0c9dd8a55c2985be688ce867666c6fdcc5c0f8f5
Revanth <97815884+ThiruvidhiRevanth@users.noreply.github.com>
2022-01-17 12:10:28 +0530

    Add files via upload

---
+++ cubeofanumber - Copy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int number,cube;
+	printf("\n enter integer value:");
+	scanf("%d",&number);
+	cube = number*number*number;
+	printf("\ncube of a number %d is =%d",number,cube);
+	return 0;
+
+}
